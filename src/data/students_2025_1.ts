5668968864925b01ed9b57c0e2db05bc0f245860
Gabriel Carvalho <ggoc.carvalho@gmail.com>
2025-11-30 12:26:02 -0300

    New Classnpm i ! (2025.1)

---
+++ src/data/students_2025_1.ts
@@ -0,0 +1,193 @@
+import { Person } from "./people";
+
+export const EduardoOliveira: Person = {
+    name: "Eduardo Oliveira Silva",
+    course: "Ciência de Dados e IA",
+    semester: "1",
+    role: "Researcher UPenn",
+    link: "",
+    photo: "pessoas/2025.1/eduardo.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const DanielSilva: Person = {
+    name: "Daniel Duarte e Silva",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "",
+    link: "http://www.linkedin.com/in/daniel-duarte-aaa707352",
+    photo: "pessoas/2025.1/daniel.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const MariaLuizaCavalcanti: Person = {
+    name: "Maria Luíza Uchoa Cavalcanti",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "Software Engineer Intern TRIL",
+    link: "https://www.linkedin.com/in/maria-lu%C3%ADza-uchoa-cavalcanti-4a3812374?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    photo: "pessoas/2025.1/uchoa.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const LarissaGondim: Person = {
+    name: "Larissa Vilasboas Gondim",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "Researcher ARIA",
+    link: "https://www.linkedin.com/in/larissa-vilasboas-gondim-332320374/",
+    photo: "pessoas/2025.1/larissa.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const PedroMenegon: Person = {
+    name: "Pedro Menegon da Graça",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/pedro-menegon-da-gra%C3%A7a-123515377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    photo: "pessoas/2025.1/menegon.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const JoaoGabrielArruda: Person = {
+    name: "João Gabriel Oliveira de Arruda",
+    course: "Ciência de Dados e IA",
+    semester: "2",
+    role: "Software Engineer Intern Cebraspe",
+    link: "https://www.linkedin.com/in/jo%C3%A3o-gabriel-oliveira-de-arruda-63a70b377/?trk=opento_sprofile_topcard",
+    photo: "pessoas/2025.1/arruda.jpeg",
+    class: "2025.1",
+    org: ["2025.1"],
+    pos: ["Membro da Organização"],
+};
+
+export const MariaMarianaCavalcante: Person = {
+    name: "Maria Mariana de Melo Cavalcante",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/maria-mariana-de-melo-cavalcante-535b3224a/",
+    photo: "pessoas/2025.1/mariana.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const MariaJullyaEloi: Person = {
+    name: "Maria Jullya de Farias Eloi",
+    course: "Engenharia da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/maria-jullya-eloi-545537377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    photo: "/pessoas/2025.1/jullya.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const AnaVictoriaFerreira: Person = {
+    name: "Ana Victória Ferreira Ribeiro",
+    course: "Engenharia da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/ana-vict%C3%B3ria-ferreira-48374238b/",
+    photo: "pessoas/2025.1/ana.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const NicoleCosta: Person = {
+    name: "Nicole Costa e Silva",
+    course: "Ciência de Dados e IA",
+    semester: "2",
+    role: "Software Engineer Intern TRIL",
+    link: "https://www.linkedin.com/in/nicole-costa-4a02a1377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    photo: "pessoas/2025.1/nicole.jpeg",
+    class: "2025.1",
+    org: ["2025.1"],
+    pos: ["Membro da Organização"],
+};
+
+export const MarianaPontes: Person = {
+    name: "Mariana Pontes da Silva Ferreira",
+    course: "Engenharia da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/mariana-ferreira-50047a2b8/",
+    photo: "pessoas/2025.1/pontes.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const ArthurTenorio: Person = {
+    name: "Arthur Moreira Tenório",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "",
+    link: "www.linkedin.com/in/",
+    photo: "pessoas/2025.1/arthur.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const LuisAugustoOliveira: Person = {
+    name: "Luis Augusto Melo de Queiroz Oliveira",
+    course: "Ciência da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/luis-augusto-queiroz?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    photo: "pessoas/2025.1/gutomp.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const MariaCeciliaSouza: Person = {
+    name: "Maria Cecilia Lopes de Souza",
+    course: "Engenharia da Computação",
+    semester: "1",
+    role: "",
+    link: "https://www.linkedin.com/in/maria-cecilia-9a95072b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    photo: "pessoas/2025.1/maria.jpeg",
+    class: "2025.1",
+    org: [],
+    pos: [],
+};
+
+export const MariaLuisaQuintela: Person = {
+    name: "Maria Luisa Moreira Quintela",
+    course: "Engenharia da Computação",
+    semester: "2",
+    role: "Software Engineer Intern TRIL",
+    link: "https://www.linkedin.com/in/maria-lu%C3%ADsa-quintela-034061267/",
+    photo: "pessoas/2025.1/quintela.jpeg",
+    class: "2025.1",
+    org: ["2025.1"],
+    pos: ["Membro da Organização"],
+};
+
+export const Joaquim: Person = {
+    name: "Joaquim Germano",
+    course: "Ciência da Computação",
+    semester: "2",
+    role: "Software Engineer Intern Dhauz",
+    link: "https://www.linkedin.com/in/joaquim-germano-a82881339/",
+    photo: "pessoas/2025.1/joaquim.jpeg",
+    class: "2025.1",
+    org: ["2025.1"],
+    pos: ["Membro da Organização"],
+};
