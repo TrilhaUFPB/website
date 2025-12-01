4b050c3b521e78870e85033f2bdbe6947e32c404
Gabriel Carvalho <ggoc.carvalho@gmail.com>
2025-12-01 18:31:33 -0300

    semester updates 2025.1

--- src/data/students_2025_1.ts
+++ src/data/students_2025_1.ts
@@ -3,7 +3,7 @@ import { Person } from "./people";
 export const EduardoOliveira: Person = {
     name: "Eduardo Oliveira Silva",
     course: "Ciência de Dados e IA",
-    semester: "1",
+    semester: "2",
     role: "Researcher UPenn",
     link: "",
     photo: "pessoas/2025.1/eduardo.jpeg",
@@ -15,7 +15,7 @@ export const EduardoOliveira: Person = {
 export const DanielSilva: Person = {
     name: "Daniel Duarte e Silva",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "3",
     role: "Software Engineer Intern TRIL",
     link: "http://www.linkedin.com/in/daniel-duarte-aaa707352",
     photo: "pessoas/2025.1/daniel.jpeg",
@@ -27,7 +27,7 @@ export const DanielSilva: Person = {
 export const MariaLuizaCavalcanti: Person = {
     name: "Maria Luíza Uchoa Cavalcanti",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "Software Engineer Intern TRIL",
     link: "https://www.linkedin.com/in/maria-lu%C3%ADza-uchoa-cavalcanti-4a3812374?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     photo: "pessoas/2025.1/uchoa.jpeg",
@@ -39,7 +39,7 @@ export const MariaLuizaCavalcanti: Person = {
 export const LarissaGondim: Person = {
     name: "Larissa Vilasboas Gondim",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "Researcher ARIA",
     link: "https://www.linkedin.com/in/larissa-vilasboas-gondim-332320374/",
     photo: "pessoas/2025.1/larissa.jpeg",
@@ -51,7 +51,7 @@ export const LarissaGondim: Person = {
 export const PedroMenegon: Person = {
     name: "Pedro Menegon da Graça",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/pedro-menegon-da-gra%C3%A7a-123515377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     photo: "pessoas/2025.1/menegon.jpeg",
@@ -75,7 +75,7 @@ export const JoaoGabrielArruda: Person = {
 export const MariaMarianaCavalcante: Person = {
     name: "Maria Mariana de Melo Cavalcante",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/maria-mariana-de-melo-cavalcante-535b3224a/",
     photo: "pessoas/2025.1/mariana.jpeg",
@@ -87,7 +87,7 @@ export const MariaMarianaCavalcante: Person = {
 export const MariaJullyaEloi: Person = {
     name: "Maria Jullya de Farias Eloi",
     course: "Engenharia da Computação",
-    semester: "1",
+    semester: "2",
     role: "Software Engineer Intern Cebraspe",
     link: "https://www.linkedin.com/in/maria-jullya-eloi-545537377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     photo: "/pessoas/2025.1/jullya.jpeg",
@@ -99,7 +99,7 @@ export const MariaJullyaEloi: Person = {
 export const AnaVictoriaFerreira: Person = {
     name: "Ana Victória Ferreira Ribeiro",
     course: "Engenharia da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/ana-vict%C3%B3ria-ferreira-48374238b/",
     photo: "pessoas/2025.1/ana.jpeg",
@@ -123,7 +123,7 @@ export const NicoleCosta: Person = {
 export const MarianaPontes: Person = {
     name: "Mariana Pontes da Silva Ferreira",
     course: "Engenharia da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/mariana-ferreira-50047a2b8/",
     photo: "pessoas/2025.1/pontes.jpeg",
@@ -135,7 +135,7 @@ export const MarianaPontes: Person = {
 export const ArthurTenorio: Person = {
     name: "Arthur Moreira Tenório",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "www.linkedin.com/in/",
     photo: "pessoas/2025.1/arthur.jpeg",
@@ -147,7 +147,7 @@ export const ArthurTenorio: Person = {
 export const LuisAugustoOliveira: Person = {
     name: "Luis Augusto Melo de Queiroz Oliveira",
     course: "Ciência da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/luis-augusto-queiroz?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     photo: "pessoas/2025.1/gutomp.jpeg",
@@ -159,7 +159,7 @@ export const LuisAugustoOliveira: Person = {
 export const MariaCeciliaSouza: Person = {
     name: "Maria Cecilia Lopes de Souza",
     course: "Engenharia da Computação",
-    semester: "1",
+    semester: "2",
     role: "",
     link: "https://www.linkedin.com/in/maria-cecilia-9a95072b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     photo: "pessoas/2025.1/maria.jpeg",
